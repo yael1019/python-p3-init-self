ee1cf402c30340fea0c04fced56f53ee1a97b699
Yael Harosh <yaelharosh01@gmail.com>
2023-04-03 20:50:05 -0400

    pass tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = 'Mutt'):
+        self.name = name
+        self.breed = breed
+    def bark(self):
+        print('Woof!')
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+fido = Dog('Fibo')
+# print(fido.name)
+fido.owner = 'Sophie'
+# print(fido.owner)
+# print(fido.favorite_toy)
+
+snoopy = Dog('Snoopy', 'Tennis Ball')
+snoopy.name
+# print(snoopy.favorite_toy)
+
